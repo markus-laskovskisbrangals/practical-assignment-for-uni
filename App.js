fb4299549386bd35d5b4a89ed08e22995a744727
emilsdaividsklavins <73535463+emilsdaividsklavins@users.noreply.github.com>
2020-10-27 19:25:55 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import Constants from 'expo-constants';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.img} source={require('./rat.png')}/>
+      <Text style={styles.parOne}>Hello World from Group G2: Emīls Daivids Kļaviņš: -!</Text>
+      <Text style={styles.parTwo}>This is my - first React Native application!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  parOne: {
+    textAlign: 'center',
+    textAlignmentsVertical: 'center',
+    color: '#4b0082',
+    fontFamily: "Arial-BoldMT",
+    fontSize: 20
+  },
+  parTwo: {
+    textAlign: 'center',
+    textAlignmentsVertical: 'center',
+    color: '#cec6da',
+    fontFamily: "Arial-BoldMT",
+    fontSize: 16
+  },
+  img: {
+    width: 50,
+    height: 50
+  }
+});

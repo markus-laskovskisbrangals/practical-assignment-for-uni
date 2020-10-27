b02c12e973de0a79ec126e385c626f276499ea5f
markus-laskovskisbrangals <markus.laskovskisbrangals@va.lv>
2020-10-27 17:34:20 +0200

    Initial commit

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text1}>Hello World from Group 2 : Markus Ļaskovskis-Brangals!</Text>
+      <Text style={styles.text2}>This is my Markus Ļaskovskis-Brangals first React Native application</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fc1',
     alignItems: 'center',
     justifyContent: 'center',
   },
